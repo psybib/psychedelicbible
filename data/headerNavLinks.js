04621ae097bf772f3726e09a794b0f6cb2a18509
psybib <psychedelicbible@proton.me>
2024-06-12 10:34:47 -0400

    headerfix

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/donate', title: 'Donate'),
+  { href: '/donate', title: 'Donate'},
+  { href: '/about', title: 'About'}
   
 ]
 
